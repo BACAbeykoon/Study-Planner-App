c192e2af1fc53cc999642281686813561bcbf510
BACAbeykoon <bacabeykoon@gmail.com>
2025-05-21 13:49:23 +0530

    create submit form and enhance home page

---
+++ study_planner_app/lib/pages/main screens/add_new_course.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:study_planner_app/widgets/custom_button.dart';
+import 'package:study_planner_app/widgets/custom_input.dart';
+
+class AddNewCourse extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _courseNameController = TextEditingController();
+  final TextEditingController _courseDescriptionController =
+      TextEditingController();
+  final TextEditingController _courseDurationController =
+      TextEditingController();
+  final TextEditingController _courseScheduleController =
+      TextEditingController();
+  final TextEditingController _courseInstructorController =
+      TextEditingController();
+
+  AddNewCourse({super.key});
+
+  void _submitForm(BuildContext context) async {
+    if (_formKey.currentState?.validate() ?? false) {
+      // Save form
+      _formKey.currentState?.save();
+      print(_courseDescriptionController.text);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Add New Course',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 5),
+
+                //description
+                const Text(
+                  'Fill in the details below to add a new course.And start managing your study planner.',
+                  style: TextStyle(color: Colors.grey, fontSize: 14),
+                ),
+                const SizedBox(height: 20),
+                CustomInput(
+                  controller: _courseNameController,
+                  labelText: 'Course Name',
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter a course name';
+                    }
+                    return null;
+                  },
+                ),
+                CustomInput(
+                  controller: _courseDescriptionController,
+                  labelText: 'Course Description',
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter a course description';
+                    }
+                    return null;
+                  },
+                ),
+                CustomInput(
+                  controller: _courseDurationController,
+                  labelText: 'Course Duration',
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter the course duration';
+                    }
+                    return null;
+                  },
+                ),
+                CustomInput(
+                  controller: _courseScheduleController,
+                  labelText: 'Course Schedule',
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter the course schedule';
+                    }
+                    return null;
+                  },
+                ),
+                CustomInput(
+                  controller: _courseInstructorController,
+                  labelText: 'Course Instructor',
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Please enter the course instructor';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                CustomElevatedButton(
+                  text: 'Add Course',
+                  onPressed: () => _submitForm(context),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- study_planner_app/lib/router.dart
+++ study_planner_app/lib/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:study_planner_app/pages/home.dart';
+import 'package:study_planner_app/pages/main%20screens/add_new_course.dart';
 
 class RouterClass {
   final router = GoRouter(
@@ -19,6 +20,15 @@ class RouterClass {
         path: "/",
         builder: (context, state) {
           return HomePage();
+        },
+      ),
+
+      //add course
+      GoRoute(
+        name: "add Course",
+        path: "/add-course",
+        builder: (context, state) {
+          return AddNewCourse();
         },
       ),
     ],
